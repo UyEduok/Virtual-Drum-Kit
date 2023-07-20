4f38f70bf19641ed1233a8ffd6cb244165ce9307
UyEduok <uyeduok@gmail.com>
2023-07-20 03:13:45 +0100

    added functionality to detect keyboard pressand the exact key pressed, and plat sound

--- index.js
+++ index.js
@@ -2,8 +2,55 @@ var buttons = document.querySelectorAll(".drum");
 
 for (n=0; n<buttons.length; n++){
     buttons[n].addEventListener("click", function (){
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
+
+        var buttonInnerHtml = this.innerHTML
+        playSound(buttonInnerHtml);
+       
     })
 }
 
+document.addEventListener('keypress', function(press){
+    playSound(press.key)
+})
+
+function playSound(sound){
+    switch (sound) {
+        case 'w':
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case 'a':
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case 's':
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case 'd':
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case 'j':
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            
+        case 'k':
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+        case 'l':
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+    
+        default:console.log(buttonInnerHtml)
+            break;
+    }
+}
